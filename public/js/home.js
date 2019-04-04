31725a466e8137ac6f0c7665e2e77af61534a69d
supriyanaidu <mksupriya@outlook.com>
2019-04-03 22:42:11 -0600

    added p5js code-3

--- public/js/home.js
+++ public/js/home.js
@@ -4,6 +4,80 @@ function setup() {
 	x = width / 2;
 	y = height;
 }
+
+function draw() {
+	background(200);
+	drawPeople();
+	// HEEL OF THE  LEFT FOOT
+	stroke(139,69,19);
+	fill(160,82,45);
+	arc(x, y-40, 14, 24, 0, PI);
+	line(x-7, y-40, x+7, y-40);
+
+	// LEFT FOOT
+	stroke(139,69,19);
+	fill(160,82,45);
+	bezier(x-7, y-45, x-20, y-90, x+35, y-90, x+7, y-45);
+	// arc(x, y-45, 22, 65, PI, 0);
+	line(x-7, y-45, x+7, y-45);
+
+	// HEEL OF THE RIGHT FOOT
+	stroke(139,69,19);
+	fill(160,82,45);
+	arc(x+35, y-50, 14, 24, 0, PI);
+	line(x+28, y-50, x+42, y-50);
+
+	// RIGHT FOOT
+	stroke(139,69,19);
+	fill(160,82,45);
+	bezier(x+28, y-55, x, y-100, x+55, y-100, x+42, y-55);
+	// arc(x, y-45, 22, 65, PI, 0);
+	line(x+28, y-55, x+42, y-55);
+
+
+
+	// HEEL OF THE  LEFT FOOT
+	stroke(139,69,19);
+	fill(160,82,45);
+	arc(x, y-120, 14, 24, 0, PI);
+	line(x-7, y-120, x+7, y-120);
+
+	// LEFT FOOT
+	stroke(139,69,19);
+	fill(160,82,45);
+	bezier(x-7, y-125, x-20, y-170, x+35, y-170, x+7, y-125);
+	// arc(x, y-45, 22, 65, PI, 0);
+	line(x-7, y-125, x+7, y-125);
+
+	// HEEL OF THE RIGHT FOOT
+	stroke(139,69,19);
+	fill(160,82,45);
+	arc(x+35, y-130, 14, 24, 0, PI);
+	line(x+28, y-130, x+42, y-130);
+
+	// RIGHT FOOT
+	stroke(139,69,19);
+	fill(160,82,45);
+	bezier(x+28, y-135, x, y-180, x+55, y-180, x+42, y-135);
+	// arc(x, y-45, 22, 65, PI, 0);
+	line(x+28, y-135, x+42, y-135);
+
+
+	textSize(25);
+	fill(50);
+	text('supriya', x+55, y-100);
+
+//   // Jiggling randomly on the horizontal axis
+//   x = x + random(-1, 1);
+//   // Moving up at a constant speed
+//   y = y - 1;
+
+//   // Reset to the bottom
+//   if (y < 0) {
+//     y = height;
+//   }
+}
+
 $('document').ready(function(){
 //const plugin = new OSC.WebsocketClientPlugin({ port: 9912 })
 //const osc = new OSC({ plugin: plugin })
@@ -24,78 +98,7 @@ $('document').ready(function(){
 
 
 
-	function draw() {
-	  background(200);
-		drawPeople();
-	  // HEEL OF THE  LEFT FOOT
-	  stroke(139,69,19);
-	  fill(160,82,45);
-	  arc(x, y-40, 14, 24, 0, PI);
-		line(x-7, y-40, x+7, y-40);
 
-		// LEFT FOOT
-	  stroke(139,69,19);
-	  fill(160,82,45);
-		bezier(x-7, y-45, x-20, y-90, x+35, y-90, x+7, y-45);
-		// arc(x, y-45, 22, 65, PI, 0);
-		line(x-7, y-45, x+7, y-45);
-
-		// HEEL OF THE RIGHT FOOT
-	  stroke(139,69,19);
-	  fill(160,82,45);
-	  arc(x+35, y-50, 14, 24, 0, PI);
-		line(x+28, y-50, x+42, y-50);
-
-		// RIGHT FOOT
-	  stroke(139,69,19);
-	  fill(160,82,45);
-		bezier(x+28, y-55, x, y-100, x+55, y-100, x+42, y-55);
-		// arc(x, y-45, 22, 65, PI, 0);
-		line(x+28, y-55, x+42, y-55);
-
-
-
-	  // HEEL OF THE  LEFT FOOT
-	  stroke(139,69,19);
-	  fill(160,82,45);
-	  arc(x, y-120, 14, 24, 0, PI);
-		line(x-7, y-120, x+7, y-120);
-
-		// LEFT FOOT
-	  stroke(139,69,19);
-	  fill(160,82,45);
-		bezier(x-7, y-125, x-20, y-170, x+35, y-170, x+7, y-125);
-		// arc(x, y-45, 22, 65, PI, 0);
-		line(x-7, y-125, x+7, y-125);
-
-		// HEEL OF THE RIGHT FOOT
-	  stroke(139,69,19);
-	  fill(160,82,45);
-	  arc(x+35, y-130, 14, 24, 0, PI);
-		line(x+28, y-130, x+42, y-130);
-
-		// RIGHT FOOT
-	  stroke(139,69,19);
-	  fill(160,82,45);
-		bezier(x+28, y-135, x, y-180, x+55, y-180, x+42, y-135);
-		// arc(x, y-45, 22, 65, PI, 0);
-		line(x+28, y-135, x+42, y-135);
-
-
-	  textSize(25);
-	  fill(50);
-	  text('supriya', x+55, y-100);
-
-	//   // Jiggling randomly on the horizontal axis
-	//   x = x + random(-1, 1);
-	//   // Moving up at a constant speed
-	//   y = y - 1;
-
-	//   // Reset to the bottom
-	//   if (y < 0) {
-	//     y = height;
-	//   }
-	}
 
 
 	  //var el = document.getElementById('server-time');
