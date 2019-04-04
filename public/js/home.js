7523a9ffcf8ac2c17cfb44caa6cbbabae191a843
supriyanaidu <mksupriya@outlook.com>
2019-04-03 21:19:25 -0600

    updated logs to reflect the variables-2

--- public/js/home.js
+++ public/js/home.js
@@ -35,9 +35,9 @@ $('document').ready(function(){
 		for(var i = 0; i < keys.length;i++){
 		   //keys[i] for key
 		   //dictionary[keys[i]] for the value
-		   console.log("x:"+keys[1]);
-			 console.log("y:"+keys[1]);
-		   console.log("name:"+faceDict[keys[i]]);
+			 console.log("name:"+keys[i]);
+		   console.log("x:"+faceDict[keys[i][1]]);
+			 console.log("y:"+faceDict[keys[i][2]]);
 		}
 	}
 
