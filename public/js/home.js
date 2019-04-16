d85ee92851468c7cbe922c8451e9c81cff44bd03
Eric Minor <ermi1253@colorado.edu>
2019-04-15 21:16:34 -0600

    magnify

--- public/js/home.js
+++ public/js/home.js
@@ -63,8 +63,8 @@ function draw() {
 		 name = keys[i];
 		 console.log("name:"+keys[i]);
 		 var faceData = faceDict[keys[i]];
-		 x = 100*faceData[0]+180;
-		 y = 100*(faceData[1])+50+cameraOffsets[faceData[2]];
+		 x = 1000*faceData[0]+180;
+		 y = 200*(faceData[1])+50+cameraOffsets[faceData[2]];
 		
 	
 	
