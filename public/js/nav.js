6f4083d0bf6480e09a3501ff603bbaf6785b69ca
supriyanaidu <mksupriya@outlook.com>
2019-04-18 00:04:53 -0600

    added music and multi camera dropdown menu

--- public/js/nav.js
+++ public/js/nav.js
@@ -2,4 +2,19 @@ window.onload = function(){
     document.getElementById("enterButton").onclick = function () {
         window.location.href = "/map";
     }
+    document.getElementById("exitButton").onclick = function () {
+        window.location.href = "/";
+    }
+    document.getElementById("musicButton").onclick = function () {
+        var music = document.getElementById("musicButton");
+        var play = 1;
+        if (play == 1){
+          music.pause();
+          play = 0;
+        }
+        else if (play == 0) {
+          music.play();
+          play = 1;
+        }
+    }
 }
